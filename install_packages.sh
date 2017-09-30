b0066d1bde22fdaad74660c4e710d99e597937ba
Joao Santos <joao@schireson.com>
2017-09-30 13:20:26 -0400

    Added list of packages to install

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,34 @@
-# Install extra packages
+#------------------------#
+# Install extra packages #
+#------------------------#
 
-sudo apt-get install autojump
+# Ubuntu packages
+sudo apt-get install build-essential ubuntu-restricted-extras
+
+# Regular packages
+sudo apt-get install aspell autojump dos2unix fdupes git gitk lmodern screen sed tree unrar unzip wget xclip zip
+
+# Python packages (use conda for most python specific packages)
+sudo apt-get install python-jedi python-pip
+
+# Compilers
+sudo apt-get install gcc gfortran
+
+# pdf & image packages
+sudo apt-get install diffpdf eog evince imagemagick meld pdftk xloadimage a2ps ps2pdf
+
+# Latex specific packages
+sudo apt-get install tex-common texlive texlive-full latexdiff
+
+# Vim specific packages
+sudo apt-get install vim vim-addon-manager vim-gnome vim-python-jedi
+# vim-nox-py2
+
+# Useful programs
+sudo apt-get install atom postgresql
+
+# Extra Programs
+#dropbox
+#google-chrome-stable
+#mysql-connector-python
+
