bf44cd5152ae39b920bc3b8396f14a7556307482
Joao Santos <joaorsantos@gmail.com>
2017-10-07 19:29:18 -0400

    Added more packages in the install file

--- install_packages.sh
+++ install_packages.sh
@@ -3,32 +3,34 @@
 #------------------------#
 
 # Ubuntu packages
-sudo apt-get install build-essential ubuntu-restricted-extras
+sudo apt-get install bash-completion build-essential gnome-tweak-tool ttf-mscorefonts-installer ttf-ubuntu-font-family ubuntu-restricted-addons ubuntu-restricted-extras
 
 # Regular packages
-sudo apt-get install aspell autojump dos2unix fdupes git gitk lmodern screen sed tree unrar unzip wget xclip zip
+sudo apt-get install aspell autojump bzip2 dos2unix fdupes git gitk gzip hyphen-en-us lmodern powerline screen sed socat tree unrar unzip wget xclip zip
 
 # Python packages (use conda for most python specific packages)
 sudo apt-get install python-jedi python-pip
 
 # Compilers
-sudo apt-get install gcc gfortran
+sudo apt-get install g++ gcc gfortran python
 
 # pdf & image packages
-sudo apt-get install diffpdf eog evince imagemagick meld pdftk xloadimage a2ps ps2pdf
+sudo apt-get install diffpdf eog evince imagemagick meld pdftk xloadimage a2ps ps2eps ps2pdf psutils
 
 # Latex specific packages
-sudo apt-get install tex-common texlive texlive-full latexdiff
+sudo apt-get install latexdiff tex-common texlive texlive-full texmaker texmaker-data
 
 # Vim specific packages
-sudo apt-get install vim vim-addon-manager vim-gnome vim-python-jedi
-# vim-nox-py2
+sudo apt-get install vim vim-gnome vim-addon-manager vim-python-jedi
+# vim-nox-py2 vim-gnome-py2
 
 # Useful programs
-sudo apt-get install atom postgresql
+sudo apt-get install atom audacious fbreader gimp postgresql transmission-common transmission-gtk
 
 # Extra Programs
 #dropbox
 #google-chrome-stable
 #mysql-connector-python
-
+#skypeforlinux
+#spotify-client
+#vlc
