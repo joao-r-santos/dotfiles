2140afd0736b8f332544b928b5636402e904e5b4
Joao Santos <joaorsantos@gmail.com>
2019-05-13 10:22:59 -0400

    Update ubuntu packages to install

--- install_packages.sh
+++ install_packages.sh
@@ -5,39 +5,35 @@
 #------------------------------#
 
 # Ubuntu packages
-sudo apt-get install bash-completion build-essential gnome-tweak-tool ttf-mscorefonts-installer ttf-ubuntu-font-family ubuntu-restricted-addons ubuntu-restricted-extras dconf-tools
+sudo apt install bash-completion build-essential gnome-tweak-tool ttf-mscorefonts-installer ttf-ubuntu-font-family ubuntu-restricted-addons ubuntu-restricted-extras dconf-tools chrome-gnome-shell
 
 # Regular packages
-sudo apt-get install aspell autojump bzip2 dos2unix fdupes git gitk gzip hyphen-en-us lmodern powerline screen sed socat tree unrar unzip wget xclip zip shellcheck thefuck
-
-# Python packages (use conda for most python specific packages)
-sudo apt-get install python-jedi python-pip
+sudo apt install aspell autojump bzip2 dos2unix fdupes git gitk gzip hyphen-en-us lmodern powerline screen sed socat tree unrar unzip wget xclip zip shellcheck thefuck
 
 # Compilers
-sudo apt-get install g++ gcc gfortran python
+sudo apt install g++ gcc gfortran
 
 # pdf & image packages
-sudo apt-get install diffpdf eog evince imagemagick meld pdftk pdfshuffler xloadimage a2ps ps2eps ps2pdf psutils cups-pdf
+sudo apt install diffpdf eog evince imagemagick meld pdftk pdfshuffler xloadimage a2ps ps2eps ps2pdf psutils cups-pdf
 
 # Latex specific packages
-sudo apt-get install latexdiff tex-common texlive texlive-full texmaker texmaker-data
+sudo apt install latexdiff tex-common texlive texlive-full texmaker texmaker-data
+
+# Python pyenv
+# sudo apt install python-jedi python-pip
+sh ./install_pyenv.sh
 
 # Vim specific packages
-sudo apt-get install vim vim-gnome vim-addon-manager vim-python-jedi
-# vim-nox-py2 vim-gnome-py2
+sudo apt install vim vim-gnome vim-addon-manager vim-python-jedi
 
 # Useful programs
-sudo apt-get install atom audacious fbreader gimp postgresql transmission-common transmission-gtk jq tmux
-pip install magic-wormhole cheat
-#sudo apt install magic-wormhole
+sudo apt install audacious fbreader gimp postgresql transmission-common transmission-gtk jq tmux
+sudo snap install cheat
 
-# Python linters
-pip install flake8 pydocstyle isort bandit
 
 # Extra Programs
 #dropbox
 #google-chrome-stable
-#mysql-connector-python
 #skypeforlinux
 #spotify-client
 #vlc
@@ -45,6 +41,11 @@ pip install flake8 pydocstyle isort bandit
 #-----------------------#
 # Install atom packages #
 #-----------------------#
+
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt update
+sudo apt install atom
 
 apm install --packages-file list_atom_packages.txt
 # If installing MagicPython, disable the built-in language-python package
